cf8551a3bf27ba870ae50391615bb44b3a81a260
System Administrator <root@Tonys-MacBook-Pro.local>
2024-09-16 15:47:03 +0530

    add children

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}><Header />
-      {children}</body>
+      <body className={inter.className}>
+        <Header children={children} />
+     </body>
     </html>
   )
 }
