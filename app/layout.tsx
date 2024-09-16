feb364454a501f2d84b75e01b9a1a65b62e7ed6a
System Administrator <root@Tonys-MacBook-Pro.local>
2024-09-16 16:00:05 +0530

    add children in header

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header children={children} />
+        <Header> {children}</Header>
+    
      </body>
     </html>
   )
